7e598fe6b703d2671e7ee95a8845f2c1f60f2965
Laguna1 <lagunazoomarket@gmail.com>
2021-12-16 19:35:49 +0200

    Add appointment controller, modefy routes

--- db/migrate/20211216155909_add_creator_name_to_visits.rb
+++ db/migrate/20211216155909_add_creator_name_to_visits.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddCreatorNameToVisits < ActiveRecord::Migration[6.1]
   def change
     add_column :visits, :creator_name, :string
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,28 +12,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_16_161540) do
-
+ActiveRecord::Schema.define(version: 20_211_216_161_540) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "appointments", force: :cascade do |t|
-    t.string "author_name"
-    t.text "recipe"
-    t.bigint "visit_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["visit_id"], name: "index_appointments_on_visit_id"
+  create_table 'appointments', force: :cascade do |t|
+    t.string 'author_name'
+    t.text 'recipe'
+    t.bigint 'visit_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['visit_id'], name: 'index_appointments_on_visit_id'
   end
 
-  create_table "visits", force: :cascade do |t|
-    t.date "date"
-    t.time "time"
-    t.string "problem"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "creator_name"
+  create_table 'visits', force: :cascade do |t|
+    t.date 'date'
+    t.time 'time'
+    t.string 'problem'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'creator_name'
   end
 
-  add_foreign_key "appointments", "visits"
+  add_foreign_key 'appointments', 'visits'
 end
