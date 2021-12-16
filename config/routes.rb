b933819c99a2dd959a4e049b37fff34a64db0c9d
Laguna1 <lagunazoomarket@gmail.com>
2021-12-16 18:57:31 +0200

    Display appointment form on the visit page only if appointment does not already given

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   root 'visits#index'
-  resources :visits
+  resources :visits do
+    resources :appointments
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
