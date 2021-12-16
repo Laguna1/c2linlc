23a390b049655beeaf75059a9456e6ee1cd42b3c
Laguna1 <lagunazoomarket@gmail.com>
2021-12-16 23:38:23 +0200

    Modify header

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@
 
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
-
+  before_action :authenticate_user!
+  
   # GET /users
   def index
     @users = User.all
