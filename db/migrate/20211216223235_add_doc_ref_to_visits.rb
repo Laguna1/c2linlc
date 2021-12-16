b77ed544a8e2bd93a19b644454406157afd4bba8
Laguna1 <lagunazoomarket@gmail.com>
2021-12-17 00:33:49 +0200

    Add Doc references to visits table

---
+++ db/migrate/20211216223235_add_doc_ref_to_visits.rb
@@ -0,0 +1,5 @@
+class AddDocRefToVisits < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :visits, :doc, foreign_key: { to_table: :users }
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_16_222610) do
+ActiveRecord::Schema.define(version: 2021_12_16_223235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,9 +45,12 @@ ActiveRecord::Schema.define(version: 2021_12_16_222610) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "creator_id"
+    t.bigint "doc_id"
     t.index ["creator_id"], name: "index_visits_on_creator_id"
+    t.index ["doc_id"], name: "index_visits_on_doc_id"
   end
 
   add_foreign_key "appointments", "visits"
   add_foreign_key "visits", "users", column: "creator_id"
+  add_foreign_key "visits", "users", column: "doc_id"
 end
